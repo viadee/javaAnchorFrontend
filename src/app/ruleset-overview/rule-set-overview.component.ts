30a0ffc4c8b71d03552740273bd005062e316734
Alex Kröker <alex.kroeker@viadee.de>
2018-08-14 17:05:43 +0200

    view anchor rule as table

--- src/app/ruleset-overview/rule-set-overview.component.ts
+++ src/app/ruleset-overview/rule-set-overview.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ConnectionInfo} from '../_models/ConnectionInfo';
 import {H2oApiService} from '../_service/h2o-api/h2o-api.service';
+import {Rule} from '../_models/Rule';
+import {ListRenderComponent} from '../_helpers/list-render.component';
+import {LocalDataSource} from 'ng2-smart-table';
 
 @Component({
   selector: 'app-ruleset-overview',
@@ -16,16 +19,53 @@ export class RuleSetOverviewComponent implements OnInit {
   model_id: string;
   frame_id: string;
 
+  rules: Array<Rule> = [];
+
+  public source: LocalDataSource;
+  settings = {
+    columns: {
+      prediction: {
+        title: 'Prediction'
+      },
+      coverage: {
+        title: 'Coverage'
+      },
+      precision: {
+        title: 'Precicsion'
+      },
+      names: {
+        title: 'Names',
+        filter: false,
+        type: 'custom',
+        renderComponent: ListRenderComponent,
+      },
+      instance: {
+        title: 'Instance',
+        filter: false,
+        type: 'custom',
+        renderComponent: ListRenderComponent,
+      },
+    },
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+    },
+    attr: {
+      class: 'table table-striped table-hover'
+    },
+  };
+
   constructor(private route: ActivatedRoute, private _h2oApi: H2oApiService) {
+    this.source = new LocalDataSource();
+
     route.queryParams.forEach(value => {
       if (value !== undefined && value.hasOwnProperty('server') && value.hasOwnProperty('model_id') && value.hasOwnProperty('frame_id')) {
         this.server = value.server;
         this.model_id = value.model_id;
         this.frame_id = value.frame_id;
 
-        this._h2oApi.getRandomRule(this.server, this.model_id, this.frame_id).subscribe((rule: any) => {
-          console.log(rule);
-        });
+        this.loadRandom();
       } else {
         // TODO fehler anzeigen oder auf die andere Seite zurückschicken
       }
@@ -33,6 +73,19 @@ export class RuleSetOverviewComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  public onSubmit() {
+    this.loadRandom();
+  }
+
+  private loadRandom() {
+    this._h2oApi.getRandomRule(this.server, this.model_id, this.frame_id).subscribe((data: any) => {
+      const rule = JSON.parse(data);
+      this.rules.push(rule);
+      console.log(rule);
+      this.source.load(this.rules);
+    });
   }
 
 }
