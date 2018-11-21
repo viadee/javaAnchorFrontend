ae7bd0f723b9c8e5d20704fc5c5bbcd525f5633c
Alex Kröker <github@alex.kroeker.me>
2018-11-21 16:03:14 +0100

    fixed safe number pipe spec test

--- src/app/_helpers/safe-number.pipe.spec.ts
+++ src/app/_helpers/safe-number.pipe.spec.ts
@@ -1,8 +1,9 @@
 import { SafeNumberPipe } from './safe-number.pipe';
+import {DecimalPipe} from '@angular/common';
 
 describe('SafeNumberPipe', () => {
   it('create an instance', () => {
-    const pipe = new SafeNumberPipe();
+    const pipe = new SafeNumberPipe(new DecimalPipe('1'));
     expect(pipe).toBeTruthy();
   });
 });
